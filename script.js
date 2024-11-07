386e4ea5526bb6ee1333b74f6dadf93827d81f44
shalini2376 <shalinisingh2376@gmail.com>
2024-11-07 13:39:45 +0530

    fixed small points

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function addTask(){
     saveData();
 }
 
+
 listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("fulfilled");
