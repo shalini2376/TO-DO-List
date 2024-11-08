8dcbf5e03eb0ae00f231da56b3f0bb3ae9db01f6
shalini2376 <shalinisingh2376@gmail.com>
2024-11-08 18:04:16 +0530

    changed some functionalities

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container") 
+const taskInput = document.querySelector(".input-box");
+const addTaskBtn = document.querySelector("#add-task");
+const listContainer = document.querySelector("#list-container")
 
-function addTask(){
-    if (inputBox.value === ''){
-        alert("You must write something!");
-    }else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+
+addTaskBtn.addEventListener("click", () => {
+    if (taskInput.value === ""){
+        alert("Enter something")
     }
-    inputBox.value = "";
-    saveData();
-}
+    else{ 
+        let todoDiv = document.createElement("div")
+        todoDiv.classList.add("todo")
+        let todoItem = document.createElement("li")
+        todoItem.innerText = taskInput.value;
+        todoDiv.appendChild(todoItem)
+        listContainer.appendChild(todoDiv);
 
-
-listContainer.addEventListener("click", function(e) {
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("fulfilled");
-        saveData();
+        const deleteBtn = document.createElement("Button");
+        deleteBtn.innerText = "Delete"
+        deleteBtn.classList.add("dltBtn") 
+        todoDiv.appendChild(deleteBtn);
     }
-    else if(e.target.tagName === "SPAN"){
+    taskInput.value = "";
+    saveData()
+})
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName == "BUTTON"){
         e.target.parentElement.remove();
         saveData();
     }
-}, false)
+})
 
 function saveData (){
-    localStorage.setItem( "data", listContainer.innerHTML);
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
 function showTask(){
